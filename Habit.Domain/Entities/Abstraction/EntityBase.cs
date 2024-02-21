4143fb51822605fa056dfa31c180496f40d37119
zhannur <jannuraidynuly@gmail.com>
2024-02-21 08:19:00 +0600

    UserImageUrl

--- Habit.Domain/Entities/Abstraction/EntityBase.cs
+++ Habit.Domain/Entities/Abstraction/EntityBase.cs
@@ -1,6 +1,13 @@
 namespace Habit.Domain.Entities.Abstraction;
 
+
+/// <summary>
+/// Базовый абстрактный класс, предоставляющий основную реализацию для сущностей.
+/// </summary>
 public abstract class EntityBase
 {
+    /// <summary>
+    /// Получает или устанавливает уникальный идентификатор сущности.
+    /// </summary>
     public Guid Id { get; set; }
 }
--- Habit.Domain/Entities/User.cs
+++ Habit.Domain/Entities/User.cs
@@ -2,12 +2,21 @@ using Habit.Domain.Entities.Abstraction;
 
 namespace Habit.Domain.Entities;
 
+/// <summary>
+/// Представляет сущность пользователя.
+/// </summary>
 public class User : EntityBase
 {
     private User()
     {
     }
     
+    /// <summary>
+    /// Инициализирует новый экземпляр класса User с указанными данными пользователя.
+    /// </summary>
+    /// <param name="firstName">Имя пользователя.</param>
+    /// <param name="lastName">Фамилия пользователя.</param>
+    /// <param name="email">Адрес электронной почты пользователя.</param>
     public User(
         string firstName,
         string lastName,
@@ -18,22 +27,37 @@ public class User : EntityBase
         Email = email;
     }
 
+    /// <summary>
+    /// Регистрирует пользователя с указанным хэшем пароля.
+    /// </summary>
+    /// <param name="passwordHash">Хэш пароля пользователя.</param>
     public void Registration(string passwordHash)
     {
         PasswordHash = passwordHash;
         IsEmailConfirmed = false;
     }
 
+    /// <summary>
+    /// Подтверждает адрес электронной почты пользователя.
+    /// </summary>
     public void ConfirmEmail()
     {
         IsEmailConfirmed = true;
     }
 
+    /// <summary>
+    /// Изменяет хэш пароля пользователя.
+    /// </summary>
+    /// <param name="passwordHash">Новый хэш пароля пользователя.</param>
     public void ChangePasswordHash(string passwordHash)
     {
         PasswordHash = passwordHash;
     }
 
+    /// <summary>
+    /// Изменяет дату рождения пользователя.
+    /// </summary>
+    /// <param name="birthDay">Новая дата рождения пользователя.</param>
     public void ChangeBirthDay(DateTime? birthDay)
     {
         if (birthDay is not null)
@@ -41,17 +65,63 @@ public class User : EntityBase
             BirthDay = birthDay;
         }
     }
+
+    /// <summary>
+    /// Изменяет URL изображения пользователя.
+    /// </summary>
+    /// <param name="imageUrl">Новый URL изображения пользователя.</param>
+    public void ChangeImage(string imageUrl)
+    {
+        ImageUrl = imageUrl;
+    }
     
+    /// <summary>
+    /// Получает имя пользователя.
+    /// </summary>
     public string FirstName { get; private set; }
     
+    /// <summary>
+    /// Получает фамилию пользователя.
+    /// </summary>
     public string LastName { get; private set; }
+    
+    /// <summary>
+    /// Получает адрес электронной почты пользователя.
+    /// </summary>
     public string Email { get; private set; }
+    
+    /// <summary>
+    /// Получает признак подтверждения адреса электронной почты пользователя.
+    /// </summary>
     public bool IsEmailConfirmed { get; private set; }
+    
+    /// <summary>
+    /// Получает дату рождения пользователя.
+    /// </summary>
     public DateTime? BirthDay { get; private set; }
+    
+    /// <summary>
+    /// Получает хэш пароля пользователя.
+    /// </summary>
     public string PasswordHash { get; private set; }
+    
+    /// <summary>
+    /// Получает URL изображения пользователя.
+    /// </summary>
+    public string? ImageUrl { get; private set; }
+    
+    /// <summary>
+    /// Получает список привычек пользователя.
+    /// </summary>
     public List<Habit>? Habits { get; private set; }
     
+    /// <summary>
+    /// Получает список подтверждений пользователя.
+    /// </summary>
     public List<UserVerify>? UserVerification { get; private set; }
     
+    /// <summary>
+    /// Получает токен обновления пользователя.
+    /// </summary>
     public RefreshToken? RefreshToken { get; private set; }
 }
