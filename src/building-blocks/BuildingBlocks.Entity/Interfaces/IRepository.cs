39a3bbfd6b61c63f7402ab469b6a58d390f3cb42
zhannur <jannuraidynuly@gmail.com>
2024-04-21 19:05:31 +0500

    Repository Base Class

---
+++ src/building-blocks/BuildingBlocks.Entity/Interfaces/IRepository.cs
@@ -0,0 +1,76 @@
+using System.Linq.Expressions;
+using BuildingBlocks.Entity.Abstraction;
+
+namespace BuildingBlocks.Entity.Interfaces;
+
+/// <summary>
+/// Интерфейс репозитория для доступа к данным.
+/// </summary>
+/// <typeparam name="T">Тип сущности.</typeparam>
+public interface IRepository<T> where T : EntityBase
+{
+    /// <summary>
+    /// Получает сущность по идентификатору асинхронно.
+    /// </summary>
+    /// <param name="id">Идентификатор сущности.</param>
+    /// <returns>Сущность.</returns>
+    IQueryable<T> GetByIdAsync(Guid id);
+    
+    /// <summary>
+    /// Получает список всех сущностей асинхронно.
+    /// </summary>
+    /// <returns>Список сущностей.</returns>
+    IQueryable<T> GetListAsync();
+    
+    /// <summary>
+    /// Получает список сущностей, удовлетворяющих предикату асинхронно.
+    /// </summary>
+    /// <param name="predicate">Предикат для фильтрации.</param>
+    /// <returns>Список сущностей.</returns>
+    IQueryable<T> GetListAsync(Expression<Func<T, bool>> predicate);
+    
+    /// <summary>
+    /// Проверяет, существует ли сущность, удовлетворяющая предикату, асинхронно.
+    /// </summary>
+    /// <param name="predicate">Предикат для фильтрации.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>True, если сущность существует, в противном случае - false.</returns>
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Добавляет сущность асинхронно.
+    /// </summary>
+    /// <param name="entity">Добавляемая сущность.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>Идентификатор добавленной сущности.</returns>
+    Task<Guid> AddAsync(T entity, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Добавляет коллекцию сущностей асинхронно.
+    /// </summary>
+    /// <param name="entities">Коллекция добавляемых сущностей.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Обновляет сущность асинхронно.
+    /// </summary>
+    /// <param name="entity">Обновляемая сущность.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Обновляет коллекцию сущностей асинхронно.
+    /// </summary>
+    /// <param name="entities">Коллекция обновляемых сущностей.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Асинхронно удаляет элемент по указанному идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор элемента для удаления.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>Задача, представляющая асинхронную операцию удаления.</returns>
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
